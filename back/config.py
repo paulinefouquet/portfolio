77415f60c3c538d477308fdbcbc8734eefaf4051
Pauline F <pauline.fouquet.dev@gmail.com>
2024-04-19 14:02:55 +0200

    modif chemein replace

--- back/config.py
+++ back/config.py
@@ -1,4 +1,4 @@
 # Saisissez votre Key obtenu sur le site edenai format "Bearer xxxxxxxxxxx...xxxx"
-EDENAI_KEY = password.key
+EDENAI_KEY = passwordkey
 PORT = 8000  # port utiliser par uvicorn
 FRONT_URL = "http://staging-pauline-portfolio/index.html"
