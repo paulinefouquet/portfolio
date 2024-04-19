b86bcbbbfe08e877f525903933d5ea70250dfe6e
Pauline F <pauline.fouquet.dev@gmail.com>
2024-04-19 16:01:29 +0200

    parametrage branche prod

--- back/config.py
+++ back/config.py
@@ -1,4 +1,4 @@
 # Saisissez votre Key obtenu sur le site edenai format "Bearer xxxxxxxxxxx...xxxx"
 EDENAI_KEY = passwordkey
 PORT = 8000  # port utiliser par uvicorn
-FRONT_URL = "http://staging-pauline-portfolio.westeurope.azurecontainer.io"
+FRONT_URL = "http://pauline-portfolio.westeurope.azurecontainer.io"
