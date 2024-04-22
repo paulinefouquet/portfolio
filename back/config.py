a5f96a0e8174e2a9a7753ebba3b669c83b59628a
Pauline F <pauline.fouquet.dev@gmail.com>
2024-04-22 14:07:27 +0200

    gestion d'environnement

--- back/config.py
+++ back/config.py
@@ -1,5 +1,11 @@
+ENVIRONMENT = "dev"
+
 # Saisissez votre Key obtenu sur le site edenai format "Bearer xxxxxxxxxxx...xxxx"
 # EDENAI_KEY = passwordkey
 AZURE_OPENAI_KEY = passwordkey
 PORT = 8000  # port utiliser par uvicorn
-FRONT_URL = "http://staging-pauline-portfolio.westeurope.azurecontainer.io"
+FRONT_URL = (
+    "http://staging-pauline-portfolio.westeurope.azurecontainer.io"
+    if ENVIRONMENT == "dev"
+    else "http://pauline-portfolio.westeurope.azurecontainer.io"
+)
